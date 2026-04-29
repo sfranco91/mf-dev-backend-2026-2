f1ac34a94daa26c487e69b737ea43056e2a4f33e
Samuel <Samuel@DESKTOP-DOVDDU2>
2026-04-29 20:20:30 -0300

    Adicionar arquivos de projeto.

---
+++ mf-dev-backend-2026/Models/Veiculo.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_dev_backend_2026.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Obrigatorio informar o nome")]
+        public string Nome { get; set; }
+        
+        [Required(ErrorMessage = "Obrigatorio informar a placa")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio informar ano de fabricacao")]
+        public int AnoFabricao { get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio informar ano do modelo")]
+        public int AnoModelo { get; set; }
+    }
+}
